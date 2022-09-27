d60fd034eafd219e3d2938b3ad3d289d375003e9
KhaledOweas <Khaled.oweas@yahoo.com>
2022-09-27 15:23:04 +0200

    update use connections string

--- src/Domain/GlobalUsings.cs
+++ src/Domain/GlobalUsings.cs
@@ -4,3 +4,6 @@ global using CCA.Domain.Enums;
 global using CCA.Domain.Events;
 global using CCA.Domain.Exceptions;
 global using CCA.Domain.ValueObjects;
+
+//dotnet ef migrations add "SampleMigration" --project src\Infrastructure --startup-project src\WebUI --output-dir Persistence\Migrations
+//dotnet ef database update --project src\Infrastructure --startup-project src\WebUI 
