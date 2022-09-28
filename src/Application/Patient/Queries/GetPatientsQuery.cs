63842d0f6c2cd104efc644e820607b6bd8c44fca
KhaledOweas <Khaled.oweas@yahoo.com>
2022-09-28 06:12:52 +0200

    Fix Patient Query

--- src/Application/Patient/Queries/GetPatientsQuery.cs
+++ src/Application/Patient/Queries/GetPatientsQuery.cs
@@ -1,9 +1,9 @@
+using CCA.Application.Patietn.Queries;
 using MediatR;
 
-namespace Application.Patient.Queries
+namespace Application.Patient.Queries;
+
+public class GetPatientsQuery : IRequest<PatientDto>
 {
-    public class GetPatientsQuery:IRequest<>
-    {
-        
-    }
+
 }
